a554985d5134d546e5b8e8dac11e0144018fa0d4
Marcos Vinícius Silva <m_vinicius94@outlook.com>
2023-02-22 09:38:40 -0300

    exportanto e importando função constroiCard

--- js/buscarVideos.js
+++ js/buscarVideos.js
@@ -1,4 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideos(evento) {
     evento.preventDefault();
@@ -6,7 +7,8 @@ async function buscarVideos(evento) {
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const buscar = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(buscar)
+    const lista = document.querySelector("[data-lista]");
+    buscar.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
